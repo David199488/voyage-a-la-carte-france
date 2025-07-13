2fcdeda886e32f6d2ead62e3f5c17f538ea880c6
David199488 <badreddinedaoud449@gmail.com>
2025-07-13 17:38:37 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import { ArrowRight, Star, Shield, Clock, Users } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import Header from '@/components/Header';
